96719acd9b5af34cd37e0bf8323cafc89c578c37
Ankit Mogha <76722619+ankit-mogha@users.noreply.github.com>
2023-09-22 16:43:59 +0530

    Update Vehicle.java

--- src/main/java/com/knoldus/cloudfunction/model/Vehicle.java
+++ src/main/java/com/knoldus/cloudfunction/model/Vehicle.java
@@ -42,4 +42,14 @@ public class Vehicle {
      */
     private Double price;
 
+     /**
+     * The quantity of the car in stock.
+     */
+    private Integer quantity;
+
+    /**
+     * The tax on the car.
+     */
+    private Double tax;
+
 }
